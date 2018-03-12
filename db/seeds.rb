464fd8fe39a24c03aba3adf3d9afa1800f990764
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-12 18:49:53 +0100

    End of day 1 airjam search page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-places = Place.create([
-  { address: "#{Faker::Address.street_address} #{Faker::Address.city}",
-    name: ,
-    stage_capacity: ,
-    equipped: ,
-    photo: ,
-    description: ,
-    price_per_hour: ,
-
-
-    }])
+puts "Cleaning database"
+Place.destroy_all
+puts "Creating 20 places"
+20.times do
+  place = Place.new(
+    user: User.first,
+    address: Faker::Address.street_address + ", " + Faker::Address.city,
+    name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
+    stage_capacity: rand(1..15),
+    equipped: true,
+    remote_photo_url: "https://kitt.lewagon.com/placeholder/cities/random",
+    description: Faker::Lorem.paragraph,
+    category: ["Blues Bar", "Buena Vista", "Macarena", "Jazz", "American Rock", "Indie"].sample,
+    price: rand(20..400)
+  )
+  place.save!
+end
+puts "Finished!"
