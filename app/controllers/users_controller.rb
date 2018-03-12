e13fa2824fa9401f577dd993a412d6d79b0f3bd8
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-12 17:37:28 +0100

    creation of divs in user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def set_user
   end
 end
