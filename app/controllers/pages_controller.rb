aaf194ac710abb68d191e7b9f7605486413cda5e
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-13 17:55:59 +0100

    fix home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,7 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if current_user
-      redirect_to places_path
-      # redirect_to user_path(current_user)
-    end
   end
 
   # def search
