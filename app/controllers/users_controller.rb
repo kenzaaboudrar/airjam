7ef5dd1e8fc2af3bb5963e81930e9fa0b671aa84
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-13 14:40:59 +0100

    edit/update/users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
+    @user = User.find(params[:id])
+    authorize @user
+    @user.update(user_params)
+    if @user.save
+      redirect_to places_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :avatar, :band_manager, :band_name, :number_of_musicians, :music_type, :band_description, :band_photo)
   end
 end
