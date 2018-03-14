f275186ffd6aa336ed8694d720331101d044fc7d
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-14 15:55:57 +0100

    versioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :places
-  has_many :reservations_as_client, source: :reservations
+  has_many :reservations
   has_many :reservations_as_owner, through: :places, source: :reservations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
