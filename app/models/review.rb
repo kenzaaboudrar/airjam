0621ff8fee58844124034469fde280e8f8610512
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-15 17:14:30 +0100

    start review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
