540bd7759eef717d13e3f33bb1ccecc4c898589f
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-16 16:02:47 +0100

    last one

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Place.destroy_all
 User.destroy_all
 
 
-puts "Creating 4 users.."
+puts "Creating 6 users.."
 nina = User.create(
   email: "nina.chanteau@hec.edu",
   password: "123456",
@@ -42,8 +42,26 @@ kenza = User.create(
   band_manager: true
 )
 
+jeremy = User.create(
+  email: "jeremy@gmail.com",
+  password: "123456",
+  first_name: "Jeremy",
+  last_name: "Merlot",
+  remote_avatar_url: "https://kitt.lewagon.com/placeholder/users/JeremyMerlot",
+  band_manager: true
+)
+
+germain = User.create(
+  email: "germain@gmail.com",
+  password: "123456",
+  first_name: "Germain",
+  last_name: "Loret",
+  remote_avatar_url: "https://kitt.lewagon.com/placeholder/users/grmnlrt",
+  band_manager: true
+)
+
 puts "Creating 10 places.."
-place = Place.create(
+place_1 = Place.create(
  user_id: nina.id,
  address: "22 Rue Bonaparte, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -52,10 +70,10 @@ place = Place.create(
  remote_photo_url: "https://kitt.lewagon.com/placeholder/cities/paris",
  description: Faker::Lorem.paragraph,
  category: ["Blues Bar", "Buena Vista", "Macarena", "Jazz", "American Rock", "Indie"].sample,
- price: 200
+ price: rand(20..400)
 )
 
-place_reserved = Place.create(
+place_2 = Place.create(
  user_id: elsa.id,
  address: "16 Villa Gaudelet, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -64,10 +82,10 @@ place_reserved = Place.create(
  remote_photo_url: "https://kitt.lewagon.com/placeholder/cities/rio",
  description: Faker::Lorem.paragraph,
  category: ["Blues Bar", "Buena Vista", "Macarena", "Jazz", "American Rock", "Indie"].sample,
- price: rand(20..400)
+ price: 200
 )
 
-place = Place.create(
+place_3 = Place.create(
  user_id: elsa.id,
  address: "36 Rue des Vinaigriers, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -79,7 +97,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_4 = Place.create(
  user_id: nina.id,
  address: "26 Rue du Borrégo, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -91,7 +109,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_5 = Place.create(
  user_id: nina.id,
  address: "10 Rue de Paradis, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -103,7 +121,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_6 = Place.create(
  user_id: elsa.id,
  address: "12 Rue de l'Ancienne Comédie, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -115,7 +133,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_7 = Place.create(
  user_id: elsa.id,
  address: "14 Rue Crespin du Gast, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -127,7 +145,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_8 = Place.create(
  user_id: nina.id,
  address: "14 Rue de Chabrol, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -139,7 +157,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_9 = Place.create(
  user_id: elsa.id,
  address: "3 Rue Campagne Première, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -151,7 +169,7 @@ place = Place.create(
  price: rand(20..400)
 )
 
-place = Place.create(
+place_10 = Place.create(
  user_id: nina.id,
  address: "150 Boulevard du Montparnasse, Paris, France",
  name: Faker::BossaNova.song + [" Bar", " Club", ""].sample,
@@ -166,11 +184,172 @@ place = Place.create(
 puts "Creating 1 reservation"
 reservation = Reservation.create(
   user_id: marion.id,
-  place_id: place_reserved.id,
+  place_id: place_2.id,
   date: "Thu, 15 Mar 2018 12:00:00 UTC +00:00",
   duration: 4,
   price: 800,
   status: "accepted"
+)
+
+puts "Creating 2 reviews per place..."
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_1.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_1.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_2.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_2.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_3.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_3.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_4.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_4.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_5.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_5.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_6.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_6.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_7.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_7.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_8.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_8.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_9.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_9.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: jeremy.id,
+  place_id: place_10.id,
+  title: "Happy birthday!",
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
+)
+
+review = Review.create(
+  user_id: germain.id,
+  place_id: place_10.id,
+  title: Faker::HowIMetYourMother.catch_phrase,
+  rating: rand(1..5),
+  content: Faker::Lorem.paragraph
 )
 
 puts "Finished!"
