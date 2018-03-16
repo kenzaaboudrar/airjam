5c422a790bd50a87566133c220c56412aa136785
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-16 17:35:41 +0100

    ?

--- app/javascript/components/notification.js
+++ app/javascript/components/notification.js
@@ -1,15 +1,14 @@
 function notify() {
   const notifications = document.querySelector('notification');
 
-  const userId = notifications.dataset.userId
+  const userId = notifications.dataset.userId;
   fetch(`/users/${userId}/notification`)
   .then(response => response.json())
   .then((data) => {
       if (data.number === 0) {
-        notifications.classList.add("transparent");
+        notifications.style.display ="none";
       } else {
-        notifications.classList.remove("transparent");
-        notifications.innerHTML = data.number;
+        notifications.style.display = "block";
       };
   });
 };
