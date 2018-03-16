5ae8fb7c16db13fabbea47920a3c0acf361594da
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-16 17:15:26 +0100

    ajax notif

--- app/javascript/components/notification.js
+++ app/javascript/components/notification.js
@@ -1,15 +1,22 @@
 function notify() {
-  const notifications = document.getElementById('notification');
+  const notifications = document.querySelector('notification');
   const userId = notifications.dataset.userId
   fetch(`/users/${userId}/notification`)
   .then(response => response.json())
   .then((data) => {
-    if (data.number === 0) {
-      notifications.style.display = "none";
-    } else {
-      notifications.style.display = "block";
-      notifications.innerHTML = data.number;
-    }
+    // if (data.number === 0) {
+    //   notifications.style.display = "none";
+    // } else {
+    //   notifications.style.display = "block";
+    // });
+      if (data.number === 0) {
+        notifications.classList.add("transparent");
+      } else {
+        notifications.classList.remove("transparent");
+        notifications.innerHTML = data.number;
+      };
+
+
   });
 };
 
